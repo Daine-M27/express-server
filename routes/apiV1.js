3309ac0fc0b99f68d0892efc44a3599a9cb68f2b
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-09-05 17:23:54 -0400

    youtube search working returns json

--- routes/apiV1.js
+++ routes/apiV1.js
@@ -3,21 +3,47 @@ var router = express.Router();
 var User = require('../models/user.js');
 var request = require('request');
 
+//youtube api data
+var GoogleApi_URL = 'https://www.googleapis.com/youtube/v3/search';
+var ApiKey = 'AIzaSyDuLlKSYLeDn53_eJqd2GtWOMmuTAMD0uw';
+
+var searchTerm = 'meditation'
+
+
+
+
+// search youtube for keywords
+router.get('/search', function(req, res, next) {
+
+   var requestURL =  GoogleApi_URL + '?'
+       + 'part=snippet&key='
+       + ApiKey
+       + '&maxResults=25&type=video&videoDuration=long&q='
+       + searchTerm;
+
+        //     part: 'snippet',
+        //     key: ApiKey,
+        //     q: 'searchTerm',
+        //     maxResults: 10,
+        //     videoDuration: 'long'
+
+    console.log(requestURL);
+
+    request(requestURL, function (error, response, body) {
+        if (error) {
+            res.send(500);
+        }
+        else {res.send(body)}
+    });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.send(200);
 });
 
-router.get('/search', function(req, res, next) {
-    request('http://www.google.com', function (error, response, body) {
-        if (error) {
-            res.send(500);
-        }
 
-        else {res.send(body)}
-    });
-});
-
+//create user
 router.post('/users', function(req, res, next) {
 
     var newUser = new User(req.body);
@@ -34,4 +60,7 @@ router.post('/users', function(req, res, next) {
 
 });
 
+
+
+//exports router
 module.exports = router;
