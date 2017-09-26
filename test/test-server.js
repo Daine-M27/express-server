0c776b96303a53815ea2c63fe8d5a619024b96cf
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-09-26 08:20:27 -0400

    travis

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const {app, runServer, closeServer} = require('../app');
+//const runsServer = require('../app');
 
 const should = chai.should();
 
