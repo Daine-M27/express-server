71cba4b408ff9c8ccb8744d45d4162041166a128
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-09-26 10:50:14 -0400

    mocha

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-if(process.env == 'production'){
-    mongoose.connect("mongodb://calmstatsdbu:OmegaRED27#@ds147044.mlab.com:47044/calm-stats-database")
-}
-else{
-    mongoose.connect('mongodb://localhost/calmStatsData');
+// if(process.env == 'production'){
+//     mongoose.connect("mongodb://calmstatsdbu:OmegaRED27#@ds147044.mlab.com:47044/calm-stats-database")
+// }
+// else{
+//     mongoose.connect('mongodb://localhost/calmStatsData');
 
+mongoose.connect("mongodb://calmstatsdbu:OmegaRED27#@ds147044.mlab.com:47044/calm-stats-database");
 }
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
