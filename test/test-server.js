5f7e3cb5d025c777a757a9f84021e74443e074e8
dainemarshall@gmail.com <dainemarshall@gmail.com>
2017-09-26 09:32:53 -0400

    travis

--- test/test-server.js
+++ test/test-server.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const {app, runServer, closeServer} = require('../app');
-// const runServer = require('../app');
-// const app = require('../app');
-// const closeServer = require('../app');
+// const {app, runServer, closeServer} = require('../app');
+const runServer = require('../app').runServer;
+const app = require('../app').app;
+const closeServer = require('../app').closeServer;
 const should = chai.should();
 
 chai.use(chaiHttp);
